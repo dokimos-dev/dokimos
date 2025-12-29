3f2f2573111fefd62f01a0fbb5a46d529f8f359d
Fabio Kapsahili <fabio.kapsahili1@gmail.com>
2025-12-29 18:50:23 +0100

    WIP

--- dokimos-server/frontend/src/main.tsx
+++ dokimos-server/frontend/src/main.tsx
@@ -1,17 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router'
-import { SWRConfig } from 'swr'
-import { router } from './routes'
-import { BreadcrumbProvider } from './lib/breadcrumb-context'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router";
+import { SWRConfig } from "swr";
+import { router } from "./routes";
+import { BreadcrumbProvider } from "./lib/breadcrumb-context";
+import { ThemeProvider } from "./lib/theme-context";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <SWRConfig value={{ revalidateOnFocus: false }}>
-      <BreadcrumbProvider>
-        <RouterProvider router={router} />
-      </BreadcrumbProvider>
-    </SWRConfig>
-  </StrictMode>,
-)
+    <ThemeProvider>
+      <SWRConfig value={{ revalidateOnFocus: false }}>
+        <BreadcrumbProvider>
+          <RouterProvider router={router} />
+        </BreadcrumbProvider>
+      </SWRConfig>
+    </ThemeProvider>
+  </StrictMode>
+);

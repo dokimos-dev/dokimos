ce21d4bca3ca40bb14c8870074751de30ba3f4a5
Fabio Kapsahili <fabio.kapsahili1@gmail.com>
2026-02-27 08:33:58 +0100

    Fix IT

--- dokimos-core/src/test/java/dev/dokimos/core/integration/AgentEvaluatorIT.java
+++ dokimos-core/src/test/java/dev/dokimos/core/integration/AgentEvaluatorIT.java
@@ -110,7 +110,7 @@ class AgentEvaluatorIT {
         judge = prompt -> {
             var params = ChatCompletionCreateParams.builder()
                     .addUserMessage(prompt)
-                    .model(ChatModel.GPT_5_1_MINI)
+                    .model(ChatModel.GPT_5_MINI)
                     .build();
             return client.chat().completions().create(params)
                     .choices().get(0).message().content().orElse("");
